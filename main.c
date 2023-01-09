dfe8b70a8fcbb5ec527ab48d2c0391ec81d9eb71
hocine abderrahmane <95042350+Abderrahmanehocine@users.noreply.github.com>
2023-01-09 09:15:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+// The solution of knapsack problem using greedy method
+// The code written by Hocine Abderrahmene
+// you can test the code in replit in this link: https://replit.com/@Abderrahmanehocine/knapsack-probelm-greedy-method?v=1
+
 #include <stdio.h>
 
 void knapsack_solution(int m, int *profit, int *weight, int size){
     int used[size];
     int cur_weight, item;
-    float total_profit;
+    float total_profit = 0;
     int i;
     for (i = 0; i < size; ++i)
         used[i] = 0;
@@ -21,21 +25,21 @@ void knapsack_solution(int m, int *profit, int *weight, int size){
         cur_weight -= weight[item];
         total_profit += profit[item];
         if (cur_weight >= 0)
-            printf("Added object %d (%d Rs., %dKg) completely in the bag. Space left: %d.\n", item + 1, profit[item], weight[item], cur_weight);
+            printf("Added object %d (%d , %dKg) completely in the bag. Space left: %d Kg.\n", item + 1, profit[item], weight[item], cur_weight);
         else
         {
             int item_percent = (int) ((1 + (float) cur_weight / weight[item]) * 100);
-            printf("Added %d%% (%d Rs., %dKg) of object %d in the bag.\n", item_percent, profit[item], weight[item], item + 1);
+            printf("Added %d%% (%d , %dKg) of object %d in the bag.\n", item_percent, profit[item], weight[item], item + 1);
             total_profit -= profit[item];
             total_profit += (1 + (float)cur_weight / weight[item]) * profit[item];
         }
     }
 
-    printf("Filled the bag with objects worth %.2f Rs.\n", total_profit);
+    printf("Filled the bag with objects worth %.2f \n", total_profit);
 }
 
 
-int main(int *argc, int **argv){
+int main(){
     int number_of_objects, max_weight;
     // scan the capacity number of the bag
     printf("Enter the max weight: ");
@@ -57,7 +61,6 @@ int main(int *argc, int **argv){
         scanf("%d",&profit[i]);
         printf("Weight: ");
         scanf("%d",&weight[i]);
-        pw[i] = (float)profit[i]/(float)weight[i];
         printf("\n");
     }
     
